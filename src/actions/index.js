b26555ef2f85a612129fc410b5a3e417407dd3b3
Anne-Julia Scheuermann <annejulia@gmail.com>
2015-10-23 23:37:54 +0200

    add drink with min and max

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ export const ADD_DRINK = 'ADD_DRINK';
 /*
  * action creators
  */
-export function addDrink(name) {
-  return { type : ADD_DRINK, payload: {name: name, min: 0, max: 0} };
+export function addDrink(name, min, max) {
+  return { type : ADD_DRINK, payload: {name, min, max} };
 }
 
--- src/components/AddDrink.js
+++ src/components/AddDrink.js
@@ -6,10 +6,28 @@ export default class AddDrink extends React.Component {
   }
 
   handleClick() {
-    const node = this.refs.name;
-    const name = node.value.trim();
-    this.props.onAddClick(name);
-    node.value = '';
+
+    const name = this.refs.name.value.trim();
+    const min  = parseInt(this.refs.min.value.trim(), 10);
+    const max  = parseInt(this.refs.max.value.trim(), 10);
+
+    if (!name || !min || !max) {
+      // throw new Error('All Fields are required!');
+      return;
+    }
+
+    this.props.onAddClick(name, min, max);
+
+    this.refs.name.value = '';
+    this.refs.min.value = '';
+    this.refs.max.value = '';
+  }
+
+  handleEnter(event) {
+    if (event.keyCode == 13) {
+      this.handleClick();
+    }
+    // console.log(event);
   }
 
   render () {
@@ -18,22 +36,22 @@ export default class AddDrink extends React.Component {
         <div className="row">
           <div className="col-md-12">
             <label className="sr-only" htmlFor="name">name</label>
-            <input type="text" ref="name" className="form-control" placeholder="neues getra:nc" />
+            <input type="text" ref="name" className="form-control" placeholder="neues getra:nc" onKeyDown={(event) => this.handleEnter(event)}/>
           </div>
         </div>
         <div className="row">
           <div className="col-md-6">
             <label className="sr-only" htmlFor="min">min</label>
-            <input type="text" ref="min" className="form-control" placeholder="min" />
+            <input type="text" ref="min" className="form-control" placeholder="min" onKeyDown={(event) => this.handleEnter(event)}/>
           </div>
           <div className="col-md-6">
             <label className="sr-only" htmlFor="max">max</label>
-            <input type="text" ref="max" className="form-control" placeholder="max" />
+            <input type="text" ref="max" className="form-control" placeholder="max" onKeyDown={(event) => this.handleEnter(event)}/>
           </div>
         </div>
         <div className="row">
           <div className="col-md-12">
-            <button onClick={() => this.handleClick()} className="btn btn-default" type="button">anfu:gen</button>
+            <button onClick={() => this.handleClick()} className="btn btn-default col-md-12" type="button">anfu:gen</button>
           </div>
         </div>
       </div>
