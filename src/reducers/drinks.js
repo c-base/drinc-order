c3c7f3fdcfa806a45d00e928630153510f4bc271
Anne-Julia Scheuermann <annejulia@gmail.com>
2015-11-08 12:58:36 +0100

    drinks: setup initial state

--- src/reducers/drinks.js
+++ src/reducers/drinks.js
@@ -1,22 +1,38 @@
 import { createReducer } from 'utils';
 import * as actions      from '../actions';
 
-const initialState = require('json!./../../config/drinks.json');
+const initialState = require('json!./../../config/state.json');
 
 // set identifier
 const generateId = (index) => 'drink-' + (index + 1);
 const drinkFactory = (drink, index) => {
-  drink.id = generateId(index);
-  drink.ist = drink.ist || null;
-  drink.istHistory = drink.istHistory || [];
+  Object.assign(drink, {
+    id         : generateId(index),
+    liter      : 0.5,
+    volume     : 0.0,
+    priceCrew  : 1.3,
+    priceAlien : 2.0,
+    ist        : null,
+    istHistory : [],
+    min        : 0,
+    max        : 0,
+    group      : 'NONE',
+    inProgram  : true,
+    ingredients: {
+      caffeine     : false,
+      coloring     : false,
+      sweeteners   : false,
+      antioxidants : false,
+      sulfites     : false
+    }
+  }, {...drink});
   return drink;
 };
-initialState.map((drink, index) => {
+initialState.drinks.map((drink, index) => {
   drinkFactory(drink, index);
 });
 
-
-export default createReducer(initialState, {
+export default createReducer(initialState.drinks, {
   [actions.DRINK_CREATE]     : (state, action) => [...state, drinkFactory(action, state.length)],
   [actions.DRINK_RECORD_IST] : (state, action) => {
     const index = state.findIndex((element) => element.id === action.id);
@@ -24,6 +40,11 @@ export default createReducer(initialState, {
       ist: action.ist,
       istHistory: [...state[index].istHistory, {ist: action.ist, date: new Date()}]
     });
+    return [...state.slice(0, index), drink, ...state.slice(index + 1)];
+  },
+  [actions.DRINK_UPDATE] : (state, action) => {
+    const index = state.findIndex((element) => element.id === action.id);
+    const drink = Object.assign({}, action);
     return [...state.slice(0, index), drink, ...state.slice(index + 1)];
   }
 });
