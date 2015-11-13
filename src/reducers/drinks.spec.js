eaeff9495e068c2e323d13dc3166629e6be4a088
Anne-Julia Scheuermann <annejulia@gmail.com>
2015-11-13 17:21:58 +0100

    drinks: tests

---
+++ src/reducers/drinks.spec.js
@@ -0,0 +1,33 @@
+import reducer           from './drinks'
+import * as actions      from '../actions';
+
+describe('drinks reducer', () => {
+
+  let initialState = [{
+    "name": "Club Mate",
+    "min": 5,
+    "max": 8,
+    "id": "drink-1",
+    "ist": null,
+    "istHistory": []
+  }]
+
+  it('DRINK_CREATE: should return the created drink', () => {
+    const state = reducer([], {type: 'DRINK_CREATE', payload: {name: 'New Drink'}});
+    expect(state[0].id).to.equal('drink-1')
+    expect(state.length).to.equal(1)
+  })
+
+  it('DRINK_RECORD_IST: should set the ist value', () => {
+    const drink = {id: 'drink-1', ist: null, istHistory: []}
+    const state = reducer([initialState], {type: 'DRINK_RECORD_IST', payload: {id: 'drink-1', ist: 23}});
+    expect(state[0].ist).to.equal(23)
+  })
+
+  it('DRINK_UPDATE: should update the a drink', () => {
+    const state = reducer(initialState, {type: 'DRINK_UPDATE', payload: {id: 'drink-1', max: 42}});
+    expect(state[0].max).to.equal(42)
+
+  })
+
+})
