3f70c274a1a113a3ce1a292415f6e778e7bc7ee2
Anne-Julia Scheuermann <annejulia@gmail.com>
2015-11-27 22:01:39 +0100

    test: use actions in test

--- src/reducers/drinks.spec.js
+++ src/reducers/drinks.spec.js
@@ -1,7 +1,7 @@
 import reducer           from './drinks'
 import * as actions      from '../actions';
 
-describe('drinks reducer', () => {
+describe('(Reducer) drinks', () => {
 
   let initialState = [{
     "name": "Club Mate",
@@ -13,19 +13,19 @@ describe('drinks reducer', () => {
   }]
 
   it('DRINK_CREATE: should return the created drink', () => {
-    const state = reducer([], {type: 'DRINK_CREATE', payload: {name: 'New Drink'}});
+    const state = reducer([], {type: actions.DRINK_CREATE, payload: {name: 'New Drink'}});
     expect(state[0].id).to.equal('drink-1')
     expect(state.length).to.equal(1)
   })
 
   it('DRINK_RECORD_IST: should set the ist value', () => {
     const drink = {id: 'drink-1', ist: null, istHistory: []}
-    const state = reducer([initialState], {type: 'DRINK_RECORD_IST', payload: {id: 'drink-1', ist: 23}});
+    const state = reducer(initialState, {type: actions.DRINK_RECORD_IST, payload: {id: 'drink-1', ist: 23}});
     expect(state[0].ist).to.equal(23)
   })
 
   it('DRINK_UPDATE: should update the a drink', () => {
-    const state = reducer(initialState, {type: 'DRINK_UPDATE', payload: {id: 'drink-1', max: 42}});
+    const state = reducer(initialState, {type: actions.DRINK_UPDATE, payload: {id: 'drink-1', max: 42}});
     expect(state[0].max).to.equal(42)
 
   })
