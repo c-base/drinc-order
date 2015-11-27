c21dc6840c76ad614f62dff105876c932602ee91
Anne-Julia Scheuermann <annejulia@gmail.com>
2015-11-27 23:56:17 +0100

    constants

---
+++ src/constants/index.js
@@ -0,0 +1,17 @@
+
+export const drinkGroups = {
+  NONE: 'NONE',
+  SODA: 'SODA',
+  BEER: 'BEER',
+  TSEC: 'TSEC',
+  COFE: 'COFE',
+  CIDR: 'CIDR',
+  CHOC: 'CHOC'
+};
+
+export const navigation = [
+  {route: '/',             name: 'carte'},
+  {route: '/inventar',     name: 'inventar'},
+  {route: '/bectellungen', name: 'bectellungen'},
+  {route: '/verwaltunc',   name: 'verwaltunc'}
+];
